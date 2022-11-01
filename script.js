e8ec509ce10d92dd605f411a1d676aca6b61eb34
brihanPenayo <brihan.penayos@gmail.com>
2022-10-31 23:31:41 -0300

    Juego Ahorcado finalizado, corregido bugs y estilos terminados

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const BTN_INICIAR = document.getElementById('btnIniciar');
 const INPUT_TEXTO = document.getElementById('inputTexto');
 const TEXTO_ADIVINAR = document.getElementById('textOculto');
-const textos = ['PERRO', 'GATO', 'LUZ', 'AGUA', 'CAMILA', 'PEPITO', 'KURO', 'GALA', 'FRANCIS'];
+const AHORCADO_IMG = document.getElementById('ahorcadoImagen');
+const GANASTE = document.getElementById('mensajeGanador');
+const textos = ['PERRO', 'GATO', 'NIÑOS', 'AGUA', 'CANILLA', 'COMPUTADORA', 'RUTA', 'REGLA', 'PROGRAMADOR', 'PROGRAMACION', 'ESTUDIOSO'];
 let textoConvertido = '';
 let textosIndice = 0;
-let fallas = 0;
-let palabra='';
+let contador = 0;
+let palabra = '';
+
+// Añadimos un eventListener al input para que lea los caracteres ingresados y comparamos
 INPUT_TEXTO.addEventListener('keyup', () => {
     let aux = '';
     let correcto = false;
-    comprobarIntentos(fallas);
     for (i = 0; i < palabra.length; i++) {
-        if (INPUT_TEXTO.value.toUpperCase() === palabra[i]) {
+        if (INPUT_TEXTO.value.toUpperCase() == palabra[i]) {
             aux += palabra[i];
             correcto = true;
         }
@@ -20,37 +23,51 @@ INPUT_TEXTO.addEventListener('keyup', () => {
         }
     }
     if (!correcto) {
-        fallas++;
+        contador++;
+        cambiarMunheco(contador);
+        if (contador === 6) {
+            INPUT_TEXTO.disabled = true;
+            GANASTE.innerHTML = '¡PERDISTE!'
+        }
     }
     INPUT_TEXTO.value = '';
     textoConvertido = aux;
-    TEXTO_ADIVINAR.innerHTML = `${textoConvertido}`;
-    console.log(fallas);
+    TEXTO_ADIVINAR.textContent = `${textoConvertido}`;
+    if (textoConvertido == palabra) {
+        INPUT_TEXTO.disabled = true;
+        GANASTE.innerHTML = '¡FELICIDADES, GANASTE!'
+    }
 });
 
-function comprobarIntentos(intentos) {
-    if (intentos >= 0) {
-        TEXTO_ADIVINAR.innerHTML = "<span>Jess</span>";
-    }
-}
-
+// Al boton iniciar le agregamos un EventListener que al hacer click selecciona texto aleatorio y comienza el juego
 document.getElementById("btnIniciar").addEventListener("click", () => {
     textoAleatorio();
     convertirTexto(palabra);
-    INPUT_TEXTO.focus();
+    BTN_INICIAR.innerHTML = "SIGUIENTE";
+    TEXTO_ADIVINAR.classList.remove("textoAnimado");
 });
 
+// Funcion que convierte el texto seleccionado a Guiones para que el usuario no lo vea
 function convertirTexto(txt) {
     textoConvertido = '';
     for (i = 0; i < txt.length; i++) {
         textoConvertido += '_';
     }
     TEXTO_ADIVINAR.innerHTML = `${textoConvertido}`;
+    INPUT_TEXTO.focus();
 }
 
+// Funcion que selecciona un texto del array aleatoriamente
 function textoAleatorio() {
-    textosIndice= Math.floor(Math.random()*textos.length);
-    palabra=textos[textosIndice];
-    alert(palabra);
+    textosIndice = Math.floor(Math.random() * textos.length);
+    palabra = textos[textosIndice];
+    INPUT_TEXTO.disabled = false;
+    contador = 0;
+    cambiarMunheco(0);
+    GANASTE.innerHTML = ''
 }
-    
+
+// Funcion que actualiza el muñeco del ahorcado
+function cambiarMunheco(n1) {
+    AHORCADO_IMG.src = `./res/img/img-${n1}.png`;
+}
